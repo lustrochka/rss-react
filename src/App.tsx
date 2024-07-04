dd8ab3b03f83b50073b0dc96de70d217f2cee6c2
Olga <kartoshkaslukom@gmail.com>
2024-07-04 22:15:05 +0200

    feat: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Component } from 'react';
 import { Page } from './components/page';
+import { ErrorBoundary } from './components/errorBoundary';
+import './App.scss';
 
-function App() {
-  return <Page />;
+class App extends Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <Page />
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
