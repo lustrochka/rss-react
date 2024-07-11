dd19e028e581c291053fa40277e29139b0e05806
Olga <kartoshkaslukom@gmail.com>
2024-07-11 15:21:47 +0200

    feat: change searchResults to function

--- src/components/searchResults.tsx
+++ src/components/searchResults.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { IResponseItem } from '../types';
 
 interface IMyProps {
@@ -6,19 +5,17 @@ interface IMyProps {
   data: IResponseItem[];
 }
 
-export class SearchResults extends Component<IMyProps> {
-  render() {
-    return (
-      <div className={this.props.class}>
-        <div className="result-container">
-          {this.props.data.map((item: IResponseItem) => (
-            <div key={item.uid.toString()} className="result-item">
-              <h3>{item.name}</h3>
-              <div>type: {item.astronomicalObjectType}</div>
-            </div>
-          ))}
-        </div>
+export function SearchResults(props: IMyProps) {
+  return (
+    <div className={props.class}>
+      <div className="result-container">
+        {props.data.map((item: IResponseItem) => (
+          <div key={item.uid.toString()} className="result-item">
+            <h3>{item.name}</h3>
+            <div>type: {item.astronomicalObjectType}</div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
