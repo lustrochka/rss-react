5a2dd7b73d2abad7b87b471b6181ac28deb42c51
Olga <kartoshkaslukom@gmail.com>
2024-07-11 19:04:27 +0200

    feat: change search to function

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,38 +1,35 @@
-import { Component } from 'react';
 import searchUrl from '../assets/drawing-2.svg';
+import { useState } from 'react';
 
 interface IMyProps {
   callback: (searchString: string) => void;
 }
 
-export class Search extends Component<IMyProps> {
-  state = {
-    searchString: localStorage.getItem('searchString') || '',
-  };
-  render() {
-    return (
-      <div className="search-block">
-        <div>Find astronomical object</div>
-        <div className="search">
-          <input
-            type="search"
-            value={this.state.searchString}
-            className="search-input"
-            onChange={(event) =>
-              this.setState({ searchString: event.target.value.trim() })
-            }
-          ></input>
-          <div
-            className="loupe"
-            onClick={() => {
-              localStorage.setItem('searchString', this.state.searchString);
-              this.props.callback(this.state.searchString);
-            }}
-          >
-            <img src={searchUrl}></img>
-          </div>
+export function Search(props: IMyProps) {
+  const [searchString, setSearchString] = useState(
+    localStorage.getItem('searchString') || ''
+  );
+
+  return (
+    <div className="search-block">
+      <div>Find astronomical object</div>
+      <div className="search">
+        <input
+          type="search"
+          value={searchString}
+          className="search-input"
+          onChange={(event) => setSearchString(event.target.value.trim())}
+        ></input>
+        <div
+          className="loupe"
+          onClick={() => {
+            localStorage.setItem('searchString', searchString);
+            props.callback(searchString);
+          }}
+        >
+          <img src={searchUrl}></img>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
