c1dd544e5effa3670807f8df8dddf29b541bf45c
Olga <kartoshkaslukom@gmail.com>
2024-07-11 15:20:16 +0200

    feat: change loader to function

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,20 +1,17 @@
-import { Component } from 'react';
 import imgUrl from '../assets/PIWO.svg';
 
 interface IMyProps {
   class: string;
 }
 
-export class Loader extends Component<IMyProps> {
-  render() {
-    return (
-      <div className={this.props.class}>
-        {[1, 2, 3, 4, 5].map((_, index) => (
-          <div key={index}>
-            <img src={imgUrl} className="bubble"></img>
-          </div>
-        ))}
-      </div>
-    );
-  }
+export function Loader(props: IMyProps) {
+  return (
+    <div className={props.class}>
+      {[1, 2, 3, 4, 5].map((_, index) => (
+        <div key={index}>
+          <img src={imgUrl} className="bubble"></img>
+        </div>
+      ))}
+    </div>
+  );
 }
