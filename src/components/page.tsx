b7f4e00c2f2677da8c33741c4e791eb8531e879d
Olga <kartoshkaslukom@gmail.com>
2024-07-23 17:29:15 +0200

    refactor: remove error button

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,24 +1,18 @@
 import { Search } from './search';
 import { CardList } from './cardList';
 import Pagination from './pagination';
-import '../App.scss';
-import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Flyout from './flyout/flyout';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { Loader } from './loader';
+import '../App.scss';
 
 export function Page() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [error, setError] = useState(false);
   const isLoading: boolean = useSelector(
     (state: RootState) => state.isLoading.isLoading
   );
-
-  useEffect(() => {
-    if (error) throw new Error('This is an error');
-  }, [error]);
 
   const changeUrl = () => {
     if (searchParams.get('details')) {
@@ -31,14 +25,6 @@ export function Page() {
     <>
       <div className="main-page" onClick={changeUrl}>
         <div className="top-section">
-          <button
-            className="error-button"
-            onClick={() => {
-              setError(true);
-            }}
-          >
-            Make an error
-          </button>
           <Search />
         </div>
         <div className="bottom-section">
